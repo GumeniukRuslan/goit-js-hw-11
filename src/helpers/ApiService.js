3f9fc2e068066409d2653784e03b98470bc5732e
GumeniukRuslan <bishob1997@gmail.com>
2023-04-24 11:37:43 +0300

    fix observer

--- src/helpers/ApiService.js
+++ src/helpers/ApiService.js
@@ -2,25 +2,26 @@ import axios from 'axios';
 
 export default class ApiService {
   constructor() {
-    this.page = 1,
-    this.searchParams = new URLSearchParams({
-      key: '35523628-8ab67868a338e2d72f9e83665',
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-      per_page: 40,
-      min_height: '180'
-    })
-
-    this.searchQ = ''
+    (this.page = 1),
+      (this.searchQ = ''),
+      (this.searchParams = new URLSearchParams({
+        key: '35523628-8ab67868a338e2d72f9e83665',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: 40,
+        min_height: '180',
+      }));
   }
 
   async getImgs() {
     try {
-      const response = await axios.get(`https://pixabay.com/api/?${this.searchParams}&page=${this.page}&q=${this.searchQ}`);
-      return response
+      const response = await axios.get(
+        `https://pixabay.com/api/?${this.searchParams}&page=${this.page}&q=${this.searchQ}`
+      );
+      return response;
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,57 +1,66 @@
-import ApiService from "./helpers/ApiService";
-import { makeMarkup } from "./helpers/makeMarkup";
+import ApiService from './helpers/ApiService';
+import { makeMarkup } from './helpers/makeMarkup';
+import scrollToStart from './helpers/scrollToStart';
 import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const imageList = document.querySelector('.js-imageList');
 const searchStart = document.querySelector('#search-form');
 const searchQuery = document.querySelector('input[name="searchQuery"]');
-const beginBtn = document.querySelector('.to-begin')
+const beginBtn = document.querySelector('.to-begin');
+const guard = document.querySelector('.js-guard');
 const apiService = new ApiService();
 let gallery = new SimpleLightbox('.js-imageList a');
+const options = {
+  root: null,
+  rootMargin: '500px',
+  treshold: 0,
+};
+const observer = new IntersectionObserver(onPagination, options);
 
 searchStart.addEventListener('submit', searchImages);
-beginBtn.addEventListener('click', () => window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-    }))
+
+beginBtn.addEventListener('click', scrollToStart);
 
 function searchImages(evt) {
   evt.preventDefault();
 
-  if (!searchQuery.value.trim()) return Notiflix.Notify.failure('Please enter your search request.');
-  
+  if (!searchQuery.value.trim())
+    return Notiflix.Notify.failure('Please enter your search request.');
+
   apiService.searchQ = searchQuery.value.trim();
   apiService.page = 1;
-  
-  apiService.getImgs().then((res) => {
-    if (!res.data.hits.length) return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-   
-    window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-    });
 
-    Notiflix.Notify.success(`Hooray! We found ${res.data.totalHits} images.`);
+  apiService.getImgs().then(res => {
+    if (!res.data.hits.length)
+      return Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+
+    scrollToStart();
+
+    Notiflix.Notify.success(
+      `Hooray! We found ${(res.data.totalHits = 500
+        ? res.data.totalHits + 20
+        : res.data.totalHits)} images.`
+    );
     imageList.innerHTML = makeMarkup(res);
     gallery.refresh();
+    beginBtn.style.display = 'block';
+    observer.observe(guard);
+  });
+}
 
-    const lastImgObserver = new IntersectionObserver(entries => {
-      const lastImg = entries[0];
-
-      if (!lastImg.isIntersecting) return;
-      apiService.page += 1;
-      apiService.getImgs().then((res) => {
-        imageList.insertAdjacentHTML('beforeend', makeMarkup(res));
-        gallery.refresh();
-      });
-
-      lastImgObserver.unobserve(lastImg.target);
-      lastImgObserver.observe(document.querySelector('.item-list:last-child'));
+function onPagination(entries, observer) {
+  if (entries[0].isIntersecting) {
+    apiService.page += 1;
+    apiService.getImgs().then(res => {
+      imageList.insertAdjacentHTML('beforeend', makeMarkup(res));
+      gallery.refresh();
+      if (!(imageList.childNodes.length % (res.data.totalHits + 20))) {
+        observer.unobserve(guard);
+      }
     });
-
-    lastImgObserver.observe(document.querySelector('.item-list:last-child'));
-  })
-  beginBtn.style.display = 'block'
-};
+  }
+}
